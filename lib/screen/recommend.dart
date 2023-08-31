c8041229a2c13e5d30579d87487139b9b4c728ed
LAPTOP-HC5ES0A4\76781 <huawei>
2023-08-31 11:42:12 +0800

    更新1

--- lib/screen/recommend.dart
+++ lib/screen/recommend.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -50,8 +51,27 @@ class _SecondPageState extends State<Recommend>
     super.dispose();
   }
 
+  Function throttle(Function fn, Duration time) {
+    bool cding = false;
+
+    return (Function fn, Duration time) {
+      if (cding) return;
+      cding = true;
+      Timer(time, () {
+        cding = false;
+      });
+    };
+  }
+
+
+
+
+
+
+
   void _togglePageVisibility() {
     setState(() {
+      print('dianle');
       _isPageVisible = !_isPageVisible;
       if (_isPageVisible) {
         _animationController.forward();
@@ -84,47 +104,46 @@ class _SecondPageState extends State<Recommend>
   List imgList = [
     {
       'path':
-      'https://img0.baidu.com/it/u=3319841190,850172708&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=731'
+          'https://img0.baidu.com/it/u=3319841190,850172708&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=731'
     },
     {
       'path':
-      'https://img1.baidu.com/it/u=2367088677,387627710&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750'
+          'https://img1.baidu.com/it/u=2367088677,387627710&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750'
     },
     {
       'path':
-      'https://img2.baidu.com/it/u=1446721905,1635020502&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500'
+          'https://img2.baidu.com/it/u=1446721905,1635020502&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500'
     },
     {
       'path':
-      'https://img2.baidu.com/it/u=2369183681,2327094715&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750'
+          'https://img2.baidu.com/it/u=2369183681,2327094715&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750'
     },
     {
       'path':
-      'https://img1.baidu.com/it/u=2756570657,2376436786&fm=253&fmt=auto&app=138&f=JPEG?w=600&h=400'
+          'https://img1.baidu.com/it/u=2756570657,2376436786&fm=253&fmt=auto&app=138&f=JPEG?w=600&h=400'
     },
     {
       'path':
-      'https://img1.baidu.com/it/u=956544463,3458766498&fm=253&fmt=auto&app=138&f=JPEG?w=892&h=500'
+          'https://img1.baidu.com/it/u=956544463,3458766498&fm=253&fmt=auto&app=138&f=JPEG?w=892&h=500'
     },
     {
       'path':
-      'https://img0.baidu.com/it/u=207708951,1766336769&fm=253&fmt=auto&app=138&f=JPEG?w=644&h=500'
+          'https://img0.baidu.com/it/u=207708951,1766336769&fm=253&fmt=auto&app=138&f=JPEG?w=644&h=500'
     },
     {
       'path':
-      'https://img0.baidu.com/it/u=1723718844,2581000878&fm=253&fmt=auto&app=138&f=JPEG?w=382&h=500'
+          'https://img0.baidu.com/it/u=1723718844,2581000878&fm=253&fmt=auto&app=138&f=JPEG?w=382&h=500'
     },
   ];
   List msgList = [
-    {'msg':'要实现文本超出两行省略的效果，只显示一行高度的文本，可以使用Text组件的maxLines和overflow属性。'},
-    {'msg':'靠靠靠靠靠靠靠靠靠靠靠靠靠'},
-    {'msg':'啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'},
-    {'msg':'水水水水水是是是是'},
-    {'msg':'嗡嗡嗡嗡嗡嗡'},
-    {'msg':'哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇'},
-    {'msg':'呃呃呃呃呃呃呃呃呃'},
-    {'msg':'日日日日日日日日日日日日日日日日日日日'},
-
+    {'msg': '要实现文本超出两行省略的效果，只显示一行高度的文本，可以使用Text组件的maxLines和overflow属性。'},
+    {'msg': '靠靠靠靠靠靠靠靠靠靠靠靠靠'},
+    {'msg': '啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊'},
+    {'msg': '水水水水水是是是是'},
+    {'msg': '嗡嗡嗡嗡嗡嗡'},
+    {'msg': '哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇哇'},
+    {'msg': '呃呃呃呃呃呃呃呃呃'},
+    {'msg': '日日日日日日日日日日日日日日日日日日日'},
   ];
 
   @override
@@ -139,12 +158,12 @@ class _SecondPageState extends State<Recommend>
         body: Container(
           height: maxHeight,
           width: maxWidth,
-          decoration:
-          BoxDecoration(border: Border.all(color: Colors.transparent, width: 0)),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.transparent, width: 0)),
           child: Stack(
             children: [
               Positioned(
-                top: _top ,
+                top: _top,
                 child: Container(
                   height: maxHeight,
                   width: maxWidth,
@@ -152,25 +171,47 @@ class _SecondPageState extends State<Recommend>
                     children: [
                       Positioned(
                         // top: _top,
-                        child: Container(
-                          height: maxHeight * 0.83 * 0.07,
-                          width: maxWidth ,
-                          color: Colors.yellow,
-                          child: TabBar(
-                            labelColor:Colors.orange,
-                            unselectedLabelColor: Colors.grey,
-                            isScrollable: true,
-                            tabs: [
-                              for (int i = 0; i < myList.length; i++)
-                                Tab(
-                                  text: myList[i]['name'],
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              child: Container(
+                                height: maxHeight * 0.83 * 0.07,
+                                width: maxWidth,
+                                color: Colors.white,
+                                child: TabBar(
+                                  labelColor: Colors.orange,
+                                  unselectedLabelColor: Colors.grey,
+                                  isScrollable: true,
+                                  tabs: [
+                                    for (int i = 0; i < myList.length; i++)
+                                      Tab(
+                                        text: myList[i]['name'],
+                                      ),
+                                  ],
                                 ),
-                            ],
-                          ),
+                              ),
+                            ),//tabbar
+                            Stack(
+                              children: [
+                                Positioned(
+                                  right: 0,
+                                  child: Container(
+                                    width: maxWidth * 0.07,
+                                    height: maxHeight * 0.83 * 0.07,
+                                    color: Colors.white ,
+                                    child: GestureDetector(
+                                      onTap: _togglePageVisibility,
+                                      child: Icon(Icons.keyboard_arrow_down),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ), // 按钮触发下拉导航
+                          ],
                         ),
                       ),
                       Positioned(
-                        top: maxHeight * 0.83 * 0.07 ,
+                        top: maxHeight * 0.83 * 0.07,
                         bottom: 0,
                         child: Container(
                           child: Container(
@@ -178,20 +219,26 @@ class _SecondPageState extends State<Recommend>
                             width: MediaQuery.of(context).size.width,
                             decoration: BoxDecoration(
                                 border: Border.all(
-                                  color: Colors.transparent,
-                                  width: 0,
-                                )),
+                              color: Colors.transparent,
+                              width: 0,
+                            )),
                             child: NotificationListener<ScrollNotification>(
                               onNotification: (notification) {
-                                // print(_top);
-                                if (notification is ScrollUpdateNotification) {//滚动条在移动中
-                                  if (notification.dragDetails != null &&
-                                      notification.dragDetails!.delta.dy > 0) {
+                                if (notification is ScrollUpdateNotification) {
+                                  //滚动条在移动中
+                                  if(notification.dragDetails != null && notification.dragDetails!.delta.dy < 0 && _top  <= -maxHeight * 0.97 * 0.06){
+                                    _top = -maxHeight * 0.97 * 0.06;
+                                    print('上了');
+                                  }else if(notification.dragDetails != null && notification.dragDetails!.delta.dy > 0 && _top >= 0) {
+                                    _top = 0;
+                                    print('下了');
+                                  }
+                                  else if (notification.dragDetails != null && notification.dragDetails!.delta.dy > 0) {
                                     setState(() {
                                       //往下拉 导航出现
                                       _top += notification.dragDetails!.delta.dy;
                                       // print(_top);
-                                      if(_top >= 0){
+                                      if (_top >= 0) {
                                         _top = 0;
                                         // print(_top);
                                       }
@@ -199,8 +246,8 @@ class _SecondPageState extends State<Recommend>
                                   } else if (notification.dragDetails != null && notification.dragDetails!.delta.dy < 0) {
                                     setState(() {
                                       //上拉，导航消失
-                                      if (_top <= -MediaQuery.of(context).size.height * 0.97 * 0.06) {
-                                        _top = -MediaQuery.of(context).size.height * 0.97 * 0.06;
+                                      if (_top <= -maxHeight * 0.97 * 0.06) {
+                                        _top = -maxHeight * 0.97 * 0.06;
                                       } else {
                                         _top += notification.dragDetails!.delta.dy;
                                       }
@@ -228,10 +275,15 @@ class _SecondPageState extends State<Recommend>
                                     crossAxisSpacing: 4,
                                     itemBuilder: (context, index) {
                                       // print(index);
-                                      String imagePath = imgList[index % 8]['path'];
-                                      String msgPath = msgList[index % 8]['msg'];
+                                      String imagePath =
+                                          imgList[index % 8]['path'];
+                                      String msgPath =
+                                          msgList[index % 8]['msg'];
                                       Image img = Image.network(imagePath);
-                                      return RecommendBlock(path:imagePath,img:img,msg:msgPath);
+                                      return RecommendBlock(
+                                          path: imagePath,
+                                          img: img,
+                                          msg: msgPath);
                                     },
                                   ),
                                   MasonryGridView.count(
@@ -241,10 +293,15 @@ class _SecondPageState extends State<Recommend>
                                     crossAxisSpacing: 4,
                                     itemBuilder: (context, index) {
                                       // print(index);
-                                      String imagePath = imgList[index % 8]['path'];
+                                      String imagePath =
+                                          imgList[index % 8]['path'];
                                       Image img = Image.network(imagePath);
-                                      String msgPath = msgList[index % 8]['msg'];
-                                      return RecommendBlock(path:imagePath,img:img,msg:msgPath);
+                                      String msgPath =
+                                          msgList[index % 8]['msg'];
+                                      return RecommendBlock(
+                                          path: imagePath,
+                                          img: img,
+                                          msg: msgPath);
                                     },
                                   ),
                                   Text('ABC'),
@@ -267,14 +324,46 @@ class _SecondPageState extends State<Recommend>
                             ),
                           ),
                         ),
+                      ), //tabbarview
+
+                      _isPageVisible
+                          ? GestureDetector(
+                        onTap: () {
+                          _togglePageVisibility();
+                        },
+                        child: Container(
+                          height: MediaQuery.of(context).size.height ,
+                          color: Colors.black54,
+                        ), //手势组件嵌套遮罩层
+                      )
+                          : Text(''), //手势组件
+
+                      AnimatedBuilder(
+                        animation: _animationController,
+                        builder: (BuildContext context, Widget? child) {
+                          return Positioned(
+                            height: _animationController.value *
+                                MediaQuery.of(context).size.height * 0.7,
+                            child: Container(
+                              height: 0,
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.red,
+                              child: Center(
+                                child: Text(
+                                  'Hello World',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ],
                   ),
-                ),),
+                ),
+              ),
             ],
           ),
-
-
         ),
       ),
     );
